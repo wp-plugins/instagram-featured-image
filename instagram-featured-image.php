f7039dd25eacd37a602992dafb66a698b27b4bb7
Qassim.Dev <Qassim.Dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-07 08:32:04 +0000

    readme instagram-featured-image
    git-svn-id: https://plugins.svn.wordpress.org/instagram-featured-image/trunk@1107480 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ instagram-featured-image.php
@@ -0,0 +1,155 @@
+<?php
+/*
+Plugin Name: Instagram Featured Image
+Plugin URI: http://wp-time.com/instagram-featured-image/
+Description: Add instagram featured image in your sidebar easily, responsive and hover animation.
+Version: 1.0.0
+Author: Qassim Hassan
+Author URI: http://qass.im
+License: GPLv2 or later
+*/
+
+/*  Copyright 2015  Qassim Hassan  (email : qassim.pay@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+// Instagram Featured Image
+class QassimInstagramFeaturedImageWidget extends WP_Widget {
+	function QassimInstagramFeaturedImageWidget() {
+		parent::__construct( false, 'Instagram Featured Image', array('description' => 'Display instagram featured image.') );
+	}
+
+	function widget( $args, $instance ) {
+		$title = apply_filters('widget_title', esc_attr($instance['title']));
+		$instagram_link = $instance['instagram_link'];
+		?>
+			<?php echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
+                <?php
+					$transient_name = $this->id;
+					$get_transient = get_transient( $transient_name );
+					
+					if ( empty( $get_transient ) ){	 
+						$transient_output = '';
+						
+						if( !empty($instagram_link) and preg_match("/(instagram.com)|(instagr.am)+/", $instagram_link) ){
+							$instagram_api	= wp_remote_get("http://api.instagram.com/oembed?url=$instagram_link");
+							$retrieve		= wp_remote_retrieve_body( $instagram_api );
+							$response		= json_decode($retrieve);
+							
+							if( preg_match('/(No Media Match)|(No URL Match)+/', $retrieve) ){
+								echo '<ul><li>Sorry, maybe error link or deleted link.</li></ul>';
+								return false;
+							}
+							
+							else{
+								$thumbnail_url	= $response->thumbnail_url;
+								$transient_output .= '<div class="WPTime-instagram-featured">';
+								$transient_output .= '<a href="'.$thumbnail_url.'">';
+    							$transient_output .= '<img src="'.$thumbnail_url.'">';
+								$transient_output .= '</a>';
+								$transient_output .= '</div>';
+							}
+						}
+						
+						else{
+							echo '<ul><li>Please enter instagram link.</li></ul>';
+						}
+						
+						echo $transient_output;
+						set_transient($transient_name, $transient_output, 1800);
+						
+					}else{
+						echo $get_transient;
+					}
+				?>
+			<?php echo $args['after_widget']; ?>
+        <?php
+	}//widget
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['instagram_link'] = strip_tags($new_instance['instagram_link']);
+		return $instance;
+	}//update
+	
+	function form( $instance ) {
+		$instance = wp_parse_args(
+			(array) $instance
+		);
+		
+		$defaults = array(
+			'title' => 'Instagram Image',
+			'instagram_link' => ''
+		);
+		
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$title = $instance['title'];
+		$instagram_link = $instance['instagram_link'];
+		?>
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>">Title:</label> 
+				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			</p>
+            
+			<p>
+				<label for="<?php echo $this->get_field_id('instagram_link'); ?>">Instagram Link:</label> 
+				<input class="widefat" id="<?php echo $this->get_field_id('instagram_link'); ?>" name="<?php echo $this->get_field_name('instagram_link'); ?>" type="text" value="<?php echo $instagram_link; ?>" />
+			</p>
+            <p>Note: if you want to change instagram link, enter new instagram link and wait 30 minutes, or delete this widget and drag and drop the widget again.</p>
+        <?php
+		
+	}//form
+	
+}
+add_action('widgets_init', create_function('', 'return register_widget("QassimInstagramFeaturedImageWidget");') );
+
+// Add wp head functions
+function QassimInstagramFeaturedImageWidget_CSS(){
+	?>
+    	<style type="text/css">
+			.WPTime-instagram-featured{
+				overflow:hidden !important;
+			}
+			.WPTime-instagram-featured a{
+				display:block !important;
+				text-decoration:none !important;
+				border:none !important;
+			}
+
+			.WPTime-instagram-featured img{
+				transition:ease-in-out 1s !important;
+				-moz-transition:ease-in-out 1s !important;
+				-o-transition:ease-in-out 1s !important;
+				-webkit-transition:ease-in-out 1s !important;
+				width:100% !important;
+				max-width:100% !important;
+				height:auto !important;
+			}
+
+			.WPTime-instagram-featured:hover img{
+				transform:scale(1.3) !important;
+				-webkit-transform:scale(1.3) !important;
+				-o-transform:scale(1.3) !important;
+				-moz-transform:scale(1.3) !important;
+				-ms-transform:scale(1.3) !important;
+			}
+		</style>
+    <?php
+}
+add_action( 'wp_head', 'QassimInstagramFeaturedImageWidget_CSS' ); 
+
+?>
---
+++ readme.txt
@@ -0,0 +1,76 @@
+﻿=== Instagram Featured Image ===
+Contributors: Qassim.Dev
+Donate link: http://j.mp/plugins_donation
+Tags: image widget, responsive, responsive widget, responsive image, featured, featured image, featured image widget, featured widget, image, images, shortcode, Post, plugin, posts, page, widget, admin, sidebar, google, twitter, comments, instagram, instagram image, instagram widget
+Requires at least: 2.8
+Tested up to: 4.1.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add instagram featured image in your sidebar easily, responsive and hover animation.
+
+== Description ==
+
+### Instagram Featured Image                    
+
+Add instagram featured image in your sidebar easily, responsive and hover animation.
+
+### The Features
+
+* Responsive image.
+* CSS3 Animation.
+* Compatible with any lightbox.
+* Easy.
+
+### Live Demo
+
+* [http://wp-time.com/instagram-featured-image/](http://wp-time.com/instagram-featured-image/)
+
+### Rate The Plugin
+
+* [Please rate Featured Image Widget plugin](https://wordpress.org/support/view/plugin-reviews/instagram-featured-image#postform)
+
+### Beautiful WordPress Themes
+
+* [Collection of 87 themes for only $69](http://j.mp/beautiful_themes)
+
+### Download Davinci WordPress Blog Theme
+
+* [Responsive and Retina WordPress blog Theme with a lot of features](http://wp-time.com/a-premium-wordpress-blog-themes/)
+
+### My Premium WordPress Themes And Plugins
+
+* [My premium themes and plugins on Creative Market.](http://j.mp/1AS73zL)
+* [My premium themes and plugins on Themeforest.](http://j.mp/Perso_themes)
+
+### About
+
+* [The plugin designed and developed by Qassim Hassan.](http://qass.im)
+* [Qassim Hassan on Twitter.](https://twitter.com/Qassim_Dev)
+
+### More Plugins
+
+* [My Plugins](https://profiles.wordpress.org/qassimdev/#content-plugins)
+
+== Installation ==
+
+### Installation
+
+1. Upload 'instagram-featured-image' folder to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to Appearance > Widgets > Instagram Featured Image.
+4. Drag and drop "Instagram Featured Image" widget and open it and enter instagram image link.
+5. Enjoy.
+
+== Frequently Asked Questions ==
+
+### FAQ
+
+* [For more questions or help, contact me.](http://qass.im/contact)
+* [Or ask me on twitter.](https://twitter.com/Qassim_Dev)
+
+== Changelog ==
+
+= 1.0.0 =
+First version.
